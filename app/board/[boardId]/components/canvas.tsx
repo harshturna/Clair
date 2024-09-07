06b4a5ae92834415a4a670b0760702fb76fd04d7
harsh turna <harshturna15@gmail.com>
2024-09-07 16:31:06 -0500

    add toolbar types and state

--- app/board/[boardId]/components/canvas.tsx
+++ app/board/[boardId]/components/canvas.tsx
@@ -1,19 +1,36 @@
 "use client";
 
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 import Info from "./info";
 import Participants from "./participants";
 import Toolbar from "./toolbar";
+import { useHistory, useCanRedo, useCanUndo } from "@liveblocks/react/suspense";
 
 interface CanvasProps {
   boardId: string;
 }
 
 const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canUndo={canUndo}
+        canRedo={canRedo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
--- types/canvas.ts
+++ types/canvas.ts
@@ -1,7 +1,114 @@
-export type CanvasState = {
-  mode: CanvasMode.None;
-};
+export type CanvasState =
+  | { mode: CanvasMode.None }
+  | { mode: CanvasMode.Pressing; origin: Point }
+  | {
+      mode: CanvasMode.Inserting;
+      layerType:
+        | LayerType.Ellipse
+        | LayerType.Note
+        | LayerType.Text
+        | LayerType.Square;
+    }
+  | { mode: CanvasMode.Resizing; initalBounds: XYWH; corner: Side }
+  | { mode: CanvasMode.Translating; current: Point }
+  | { mode: CanvasMode.SelectionNet; origin: Point; current?: Point }
+  | { mode: CanvasMode.Pencil };
 
 export enum CanvasMode {
   None,
+  Pressing,
+  SelectionNet,
+  Translating,
+  Inserting,
+  Resizing,
+  Pencil,
+}
+
+export type Color = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type Camera = {
+  x: number;
+  y: number;
+};
+
+export enum LayerType {
+  Square,
+  Ellipse,
+  Path,
+  Text,
+  Note,
+}
+
+export type SquareLayer = {
+  type: LayerType.Square;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type EllipseLayer = {
+  type: LayerType.Ellipse;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type PathLayer = {
+  type: LayerType.Path;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  points: number[][];
+  value?: string;
+};
+
+export type TextLayer = {
+  type: LayerType.Text;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type NoteLayer = {
+  type: LayerType.Note;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type Point = {
+  x: number;
+  y: number;
+};
+
+export type XYWH = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+export enum Side {
+  Top = 1,
+  Bottom = 2,
+  Left = 4,
+  Right = 8,
 }
