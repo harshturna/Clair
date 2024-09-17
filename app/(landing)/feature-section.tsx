c67de009cc2c59ed61af913d7a5ca36651fdb244
harsh <harshturna15@gmail.com>
2024-09-17 04:15:14 -0500

    fix padding

--- app/(landing)/feature-section.tsx
+++ app/(landing)/feature-section.tsx
@@ -92,7 +92,7 @@ const FeatureSection = () => {
 
   return (
     <>
-      <div className="pt-20  xl:pt-24 flex justify-center items-center flex-col ">
+      <div className="flex justify-center items-center flex-col ">
         <div className="text-3xl w-3/4 xl:text-5xl font-medium xl:w-1/2 text-center">
           Supercharge Your Collaboration
         </div>
